93179b502278d0e5bd4aa429690cd48151e721f2
Jessica Bohn <bohnjessica15@gmail.com>
2016-09-16 11:29:01 -0500

    Change location type to just location
    Since we're only doing the MVP versions of our game and only
    focusing on bars instead of any other category, we only need the
    actual location of where the group wants to play the game, which is
    the zipcode of that area for now.

--- db/migrate/20160915204154_create_games.rb
+++ db/migrate/20160915204154_create_games.rb
@@ -2,7 +2,7 @@ class CreateGames < ActiveRecord::Migration[5.0]
   def change
     create_table :games do |t|
       t.string :name
-      t.string :location_type, default: "bars"
+      t.string :location, default: "78701"
       t.integer :group_id
 
       t.timestamps
