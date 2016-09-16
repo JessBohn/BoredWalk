eddf97d4f5ccd2fc0dbce5fc424034f45ab5ca53
kapatton90 <kapatton19@gmail.com>
2016-09-16 10:40:34 -0500

    Create a form to enter in location that will then search for bars in that location. Also created an index page to show all bar locations associated with each bar.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,1 +1,55 @@
-games_controller.rb
+class GamesController < ApplicationController
+
+
+  def index
+    @games = Game.all
+  
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    user = User.find(current_user)
+    @game = user.games.new(game_params)
+    if @game.save
+      redirect_to @game
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @game = Game.find(params[:id])
+    response = Yelp.client.search(@game.location, { term: 'bar' })
+    @businesses = response.businesses
+    render :index
+  end
+
+  def edit
+    @game = Game.find(params[:id])
+  end
+
+  def update
+    @game = Game.find(params[:id])
+    if @game.update(game_params)
+      redirect_to @game
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @game = Game.find(params[:id])
+    @game.comments.destroy
+    @game.destroy
+    redirect_to games_path
+  end
+
+  private
+  def game_params
+    params.require(:game).permit(:name, :location)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 	resources :locations
+	resources :games
 	post '/search' => 'locations#search'
 end
