516c5595987c3324e791aad38966999440e9f964
kapatton90 <kapatton19@gmail.com>
2016-09-16 12:35:22 -0500

    Get the correct number of bars to list.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -22,8 +22,26 @@ class GamesController < ApplicationController
 
   def show
     @game = Game.find(params[:id])
-    response = Yelp.client.search(@game.location, { term: 'bar' })
+    response = Yelp.client.search(@game.location, { term: 'bar'})
     @businesses = response.businesses
+    response2 = Yelp.client.search(@game.location, { term: 'bar'})
+    @businesses2 = response2.businesses[0..1]
+    @businesses.each do |business|
+      @new_bar = Bar.find_or_create_by(name: business.name,
+      address: business.location.address,
+      rating: business.rating,
+      website: business.url,
+      photo: business.image_url)
+      @game.bars << @new_bar
+    end
+    @businesses2.each do |business|
+      @new_bar = Bar.find_or_create_by(name: business.name,
+      address: business.location.address,
+      rating: business.rating,
+      website: business.url,
+      photo: business.image_url)
+      @game.bars << @new_bar
+    end
     render :index
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20160916025410) do
     t.string   "name"
     t.string   "address"
     t.integer  "rating"
+    t.string   "website"
+    t.string   "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
