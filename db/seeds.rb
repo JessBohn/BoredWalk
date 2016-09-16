3ad1583cf766a10cf9e709357069ff8667eafd5c
Jason Pelletier <jpelletier19@me.com>
2016-09-16 10:21:05 -0500

    add board_spots column to users to make tracking board spots visited.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+player1 = User.create(name: "Kim", email: "kim@kim.com", password: 12345)
+player2 = User.create(name: "Jay", email: "jay@jay.com", password: 12345)
+player3 = User.create(name: "Jess", email: "jess@jess.com", password: 12345)
+player4 = User.create(name: "Sean", email: "sean@sean.com", password: 12345)
+
+group1 = Group.create(name: "Team Supa Dupa Fly")
+
